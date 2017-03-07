800d74e2c0cd1755e888ec6dd10505e85b15bbfa
Lutz Thies <lutz.thies@tu-dresden.de>
2017-03-07 14:59:09 +0100

    added default execution via __main__

--- src/main.py
+++ src/main.py
@@ -4,3 +4,6 @@ from Planet import Direction, Planet
 def run():
     # the execution of all code shall be started from within this function
     print("Hello World!")
+
+if __name__ == '__main__':
+    run()
